6d9eb964449f9bb0b50561b806d9b6289ce17183
Ciro Rodriguez <cirorod0103@gmail.com>
2025-03-15 16:18:08 -0300

    Feat: Se definieron las clases

--- Blog/Core/models.py
+++ Blog/Core/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def str(self):
+        return f"{self.nombre} {self.apellido}"
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField()
+    profesion = models.CharField(max_length=100)
+
+    def str(self):
+        return f"{self.nombre} {self.apellido} - {self.profesion}"
+
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=100)
+    profesor = models.CharField(max_length=100)
+    estudiantes = models.CharField(max_length=100)
+    numero_curso = models.IntegerField(max_length=100)
+    fecha_inicio = models.DateField(max_length=15)
+    fecha_fin = models.DateField(max_length=15)
+    def str(self):
+        return f"Nombre: {self.nombre} \n Profesor: {self.profesor} \n Comision: {self.numero_curso} \n  Fecha inicio: {self.fecha_inicio} \n Fecha fin: {self.fecha_fin}"
